318bb3a8ebfd4f9ffae4bb573ba635789f892c30
hananhafez <hanan.hafez12@gmail.com>
2017-04-05 17:37:08 +0200

    components main structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,30 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { FriendsComponent } from './friends/friends.component';
+import { GroupsComponent } from './groups/groups.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AddorderComponent } from './addorder/addorder.component';
+import { VieworderComponent } from './vieworder/vieworder.component';
+import { HeaderComponent } from './header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    FriendsComponent,
+    GroupsComponent,
+    OrdersComponent,
+    AddorderComponent,
+    VieworderComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
