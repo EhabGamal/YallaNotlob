c9e4ab115540fbc6fe546725991d2ef8f4f1dd96
Ehab Gamal <ehab.g.sh@gmail.com>
2017-04-06 13:27:15 +0200

    Replacing MaterializeCSS static files with MaterialzieModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+//import { MaterialModule } from '@angular/material';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 //import { SignupComponent } from './signup/signup.component';
@@ -34,7 +35,8 @@ import { HeaderComponent } from './header.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    //MaterialModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
