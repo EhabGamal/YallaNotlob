26f9d1e44617170b9adf8521c87eb794a6496f05
Ehab Gamal <ehab.g.sh@gmail.com>
2017-04-07 20:02:32 +0200

    Bulding Groups Component Structure

--- src/app/groups/groups.component.ts
+++ src/app/groups/groups.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { GroupsService } from '../services/groups.service'
+import { AppService } from '../services/app.service'
 
 @Component({
   selector: 'app-groups',
   templateUrl: './groups.component.html',
-  styleUrls: ['./groups.component.css']
+  styleUrls: ['./groups.component.css'],
+  providers: [ GroupsService, AppService ]
 })
 export class GroupsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppService, private groupsService: GroupsService) { }
 
   ngOnInit() {
+    console.log(this.appService.user);
+    //this.groupsService.getAll(this.appService.user.id).subscribe()
   }
 
 }
