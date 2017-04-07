b4aadbbfc84fd89596d89479d68219c58dea11e2
Ehab Gamal <ehab.g.sh@gmail.com>
2017-04-07 20:00:50 +0200

    Implementing Signup using Signup Service

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,16 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from "@angular/router";
+import { SignupService } from "../services/signup.service";
+import { AppService } from "../services/app.service";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  providers: [ SignupService, AppService ]
 })
 export class SignupComponent implements OnInit {
 
-  constructor( private router: Router) { }
+  user = {
+    email: '',
+    password: '',
+    rpassword: '',
+    firstName: '',
+    lastName: ''
+  };
+  msg: string = '';
+  loading: boolean = false;
+  constructor( private router: Router, private signupService: SignupService, private appService: AppService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit( form: NgForm ){
+    if(this.user.password === this.user.rpassword){
+      console.log(this.user);
+      this.msg = '';
+      this.loading = true;
+      this.signupService.register(this.user).subscribe(
+        (data: any) => {
+          console.log(data);
+          localStorage.setItem('token', data.token);
+          this.appService.setLoggedin(true);
+          this.appService.user = data.user;
+          this.router.navigate(['/']);
+          this.loading = false;
+        },
+        (error: any) => {
+          this.msg = error.json().error;
+          this.loading = false;
+        },
+        () => {
+          this.loading = false;
+        }
+      );
+    }else{
+      this.msg = 'Password does not match the confirm!';
+    }
   }
 
 }
