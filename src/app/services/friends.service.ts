14c162d94b6837fa76fd4c1342d68ff36c96c271
Ehab Gamal <ehab.g.sh@gmail.com>
2017-04-09 06:10:43 +0200

    Added Friends Service

---
+++ src/app/services/friends.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, Inject } from '@angular/core';
+import { APP_CONFIG, AppConfig } from '../app.config';
+import { Headers, Http, Response } from '@angular/http';
+import { AppService } from '../services/app.service'
+import 'rxjs/Rx';
+
+@Injectable()
+export class FriendsService {
+
+  constructor(@Inject(APP_CONFIG) private config: AppConfig, private http: Http, private appService: AppService) { }
+
+  getAll(id: string){
+    const headers = new Headers({ 'Authorization': this.appService.token});
+    return this.http.get(this.config.apiEndpoint+'users/'+id+'/friends',{headers: headers})
+      .map((response: Response) => response.json());
+  }
+  addFriend(email: string){
+    //const body = JSON.stringify({user:email});
+    const headers = new Headers({ 'Content-Type': 'application/json','Authorization': this.appService.token});
+    return this.http.post(this.config.apiEndpoint+'users/'+email+'/friend',{},{headers: headers})
+      .map((response: Response) => response.json());
+  }
+  unFriend(id: string){
+    //const body = JSON.stringify({user:email});
+    const headers = new Headers({ 'Content-Type': 'application/json','Authorization': this.appService.token});
+    return this.http.post(this.config.apiEndpoint+'users/'+id+'/unfriend',{},{headers: headers})
+      .map((response: Response) => response.json());
+  }
+
+}
