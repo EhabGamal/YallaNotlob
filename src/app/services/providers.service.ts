b37759e8cd3c50bbed75b405ce5c7560e9a3a745
Ehab Gamal <ehab.g.sh@gmail.com>
2017-04-11 00:19:57 +0200

    Added Providers service

---
+++ src/app/services/providers.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, Inject } from '@angular/core';
+import { APP_CONFIG, AppConfig } from '../app.config';
+import { Headers, Http, Response } from '@angular/http';
+import { AppService } from '../services/app.service'
+import 'rxjs/Rx';
+
+@Injectable()
+export class ProvidersService {
+
+  constructor(@Inject(APP_CONFIG) private config: AppConfig, private http: Http, private appService: AppService) { }
+
+  getAll(){
+    const headers = new Headers({ 'Content-Type': 'application/json','Authorization': this.appService.token});
+    return this.http.get(this.config.apiEndpoint+'providers/',{headers: headers})
+      .map((response: Response) => response.json());
+  }
+
+}
