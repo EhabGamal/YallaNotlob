c79c365fc077184a463253bd770687602698d0f0
Ehab Gamal <ehab.g.sh@gmail.com>
2017-04-12 23:53:09 +0200

    Edit Profile Modal prepared

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, ViewChild, OnInit} from '@angular/core';
+import { AppService } from '../services/app.service';
 import { MaterializeAction } from 'angular2-materialize';
 
 @Component({
@@ -10,10 +11,31 @@ export class HomeComponent implements OnInit {
 
   @ViewChild('modal') modal;
   modalMsg = {msg:"Testing again", icon:'person', warning:true};
+  private profileModalActions = new EventEmitter<string|MaterializeAction>();
+  private friendsActivities: any = [];
+  private latestActivities: any = [];
+  private profile: any = this.appService.user;
+  private newProfile = this.profile;
 
-  constructor() { }
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
+  }
+
+  editProfile(){
+    this.profileModalActions.emit({action:"modal",params:['open']});
+  }
+
+  saveProfile(){
+
+  }
+
+  closeProfileModal(){
+    this.profileModalActions.emit({action:"modal",params:['close']});
+  }
+
+  resetProfile(){
+    this.newProfile = this.profile;
   }
 
 }
