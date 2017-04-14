b29f2c62d1a3bc8509f3ea9310f7975a3dacefbb
Ehab Gamal <ehab.g.sh@gmail.com>
2017-04-14 23:58:13 +0200

    Partial Implementation to ViewOrder Component

--- src/app/vieworder/vieworder.component.ts
+++ src/app/vieworder/vieworder.component.ts
@@ -1,19 +1,84 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
+import { AppService } from '../services/app.service';
+import { ProvidersService } from '../services/providers.service';
+import { OrdersService } from '../services/orders.service';
+import { FriendsService } from '../services/friends.service';
 
 @Component({
   selector: 'app-vieworder',
   templateUrl: './vieworder.component.html',
-  styleUrls: ['./vieworder.component.css']
+  styleUrls: ['./vieworder.component.css'],
+  providers: [ ProvidersService, OrdersService, FriendsService ]
 })
 export class VieworderComponent implements OnInit {
 
-  constructor( private router: ActivatedRoute ) { }
+  private loading: number = 1;
+  private orderID: string = '';
+  private order: any = {id:1,name:'Breakfast Order'};
+  private availableItems: any = [];
+  private orderItems: any = [];
+  private myItems: any = [];
+  private newItem: any = {
+    item:'',
+    amount:0,
+    comment:''
+  };
+  private itemPrice: number = 0;
+  private total: number = 0;
+
+  constructor( private router: ActivatedRoute, private appService: AppService, private ordersService: OrdersService, private providersService: ProvidersService, private friendsService: FriendsService ) { }
 
   ngOnInit() {
     this.router.params.subscribe(
-      (data) => { console.log(data) }
+      (params) => { console.log(params['id']); this.orderID = params['id']; this.getOrder(); this.loading--; }
     )
+  }
+
+  getOrder(){
+    this.ordersService.getOrder(this.orderID).subscribe(
+      (data) => { console.log(data); this.order = data; console.log(this.order); this.getAvailableItems(); },
+      (error) => { console.log(error); }
+    )
+  }
+
+  getAvailableItems(){
+    this.providersService.getItems(this.order.provider).subscribe(
+      (data) => { console.log(data); this.availableItems = data; this.setItemPrice(); },
+      (error) => { console.log(error); }
+    );
+  }
+
+  changeItem(item){
+    this.newItem.item = item;
+    console.log(this.newItem);
+  }
+
+  addItem(id: string){
+
+  }
+
+  removeItem(id: string){
+
+  }
+
+  setItemPrice(){
+    //return this.availableItems.filter((item) => item._id == id).map((item) => item.price)
+    this.order.items.map((item) => {
+      console.log('item');
+      console.log(item);
+      item.price = this.availableItems.filter((available) => available._id == item.item).map((available) => { console.log('available');console.log(available); return available.price;});
+    })
+  }
+
+  calcItemPrice(){
+    //this.itemPrice = this.newItem.amount * this.availableItems.filter((item) => item._id == this.newItem.item).map((item) => item.price);
+    //this.calcTotal();
+  }
+
+  calcTotal(){
+    this.total = this.itemPrice;
+    this.order.items.filter((item) => item.orderBy == this.appService.user._id).map((item) => { this.total += item.price * item.amount; })
   }
 
 }
