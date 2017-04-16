daeed5e6db6ea800ca07a32007ed635a0625c236
salamaashoush <salamaashoush@gmail.com>
2017-04-16 18:33:20 +0200

    Merge branch 'master' of github.com:EhabGamal/YallaNotlob
    # Conflicts:
    #	src/app/parts/navbar/navbar.component.ts

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,14 +1,12 @@
 import {Injectable} from '@angular/core';
-
 @Injectable()
 export class AppService {
-
-  constructor() { }
 
   private loggedin = false;
   public user: any = JSON.parse(localStorage.getItem('user'));
   public token: string = localStorage.getItem('token');
-
+  constructor() {
+  }
   checkStatus(): boolean {
     return this.loggedin;
   }
